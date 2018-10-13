8cc5bdf7dded8c21caba519fad29967602b7ae7c
yanan lee <liyananhappy@sina.cn>
2018-10-13 21:24:26 +0800

    update

--- routes/index1.js
+++ routes/index1.js
@@ -1,6 +1,5 @@
 var express = require('express')
 var router = express.Router()
-var http = require('http')
 var cheerio = require('cheerio')
 var request = require('request')
 var mysql = require('mysql')
